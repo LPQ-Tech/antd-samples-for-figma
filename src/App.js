690a202b9994023215322a8af838234abfe42929
Dong Tan Nguyen <dong.ng.tan@gmail.com>
2023-03-30 00:16:38 +0700

    Add antd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, ConfigProvider, theme, Card, Row, Col } from "antd";
+import "antd/dist/reset.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <ConfigProvider
+    theme={{
+      algorithm: theme.darkAlgorithm,
+    }}
+  >
+    <Row gutter={16}>
+      <Col span={8}>
+        <Card title="Card title" bordered={false}>
+          <Button>Click me</Button>
+          <Button type="ghost">Gosht</Button>
+          <Button type="text">Text</Button>
+          <Button disabled>Disabled</Button>
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Card title" bordered={false}>
+        <Button type="primary" size="large">Click me</Button>
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Card title" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+    </Row>
+  </ConfigProvider>
+);
 
 export default App;
